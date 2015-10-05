e6db1329f9cb61d50ec9c4e6076e41c0f5f3e27d
Santiago <scuellar@princeton.edu>
2015-10-05 15:35:27 -0400

    Adding threaded counter example.

--- progs/threaded_counter.c
+++ progs/threaded_counter.c
@@ -1,28 +1,60 @@
 #include <pthread.h>
 
-void *incr();
 
-pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-int  counter = 0;
+/* the lock*/
+pthread_mutex_t ctr_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/* The counter part */
+
+int zero = 0;
+int *ctr = &zero;
+
+void reset() {
+  int* ctr0 = ctr;
+  *ctr0 = 0;
+}
+
+void *incr() {
+   pthread_mutex_lock( &ctr_mutex );
+   int* ctr0 = ctr;
+   int t = *ctr0;
+   *ctr0 = t +2;
+   /* printf("Counter is: %d \n", *ctr); */
+   pthread_mutex_unlock( &ctr_mutex );
+}
+
+int read() {
+  int* ctr0 = ctr;
+  int t = *ctr0;
+  return t;
+}
+
+/* The threaded part */
+void *thread_incr(void *arg) {
+   pthread_mutex_t *thread_mutex_ptr = (pthread_mutex_t *)arg;
+   incr();
+   pthread_mutex_unlock( thread_mutex_ptr );
+}
 
 int  main(void)
 {
-   int rc1, rc2;
+  
+   /* ctr = 0 */
+   reset();
+   int rc1;
+   pthread_mutex_t thread_mutex = PTHREAD_MUTEX_INITIALIZER;
+   pthread_mutex_lock( &thread_mutex );
+   pthread_t thread1;
+   /* Spawn */
+   rc1=pthread_create( &thread1, NULL, thread_incr, &thread_mutex);
    
-   pthread_t thread1, thread2;
+   /*Local incr */
+   incr();
 
-   rc1=pthread_create( &thread1, NULL, &incr, NULL);
-
-   rc2=pthread_create( &thread2, NULL, &incr, NULL);
-   pthread_join( thread1, NULL);
-   pthread_join( thread2, NULL); 
-
-   return 1;
-}
-
-void *incr()
-{
-   pthread_mutex_lock( &mutex1 );
-   counter++;
-   pthread_mutex_unlock( &mutex1 );
+   /*JOIN */
+   pthread_mutex_lock( &thread_mutex );
+   /* printf("I'm done with a final counter of: %d\n", *ctr); */
+    
+   int t = read();
+   return t;
 }
--- progs/verif_threaded_counter.v
+++ progs/verif_threaded_counter.v
@@ -3,11 +3,29 @@ Load threaded_counter.
 
 Local Open Scope logic.
 
-Definition t_struct_mutex1 := Tstruct _mutex1 noattr.
-Definition t_struct_counter := Tstruct _counter noattr.
 
-Definition incr_spec :=
- DECLARE _main
+Instance CompSpecs : compspecs.
+Proof. make_compspecs prog. Defined.  
+
+Definition t_struct_mutex1 := Tstruct _ctr_mutex noattr.
+
+Definition t_struct_zero := Tstruct _zero noattr.
+Definition t_struct_ctr := Tstruct _ctr noattr.
+
+Definition reset_spec :=
+ DECLARE _reset
+  WITH z : int, addof_ctr: val, ctr: val
+  PRE  [] 
+        PROP ()
+        LOCAL(gvar _ctr addof_ctr)
+        SEP(`(data_at Ews tint (repinj tint z) ctr);`(data_at Ews (tptr tint) ctr addof_ctr))
+  POST [tvoid]
+        PROP()
+        LOCAL()
+        SEP(`(data_at Ews tint (repinj tint Int.zero) ctr);`(data_at Ews (tptr tint) ctr addof_ctr)).
+
+Definition read_spec :=
+ DECLARE _read
   WITH z : int, addof_ctr: val, ctr: val
   PRE  [] 
         PROP ()
@@ -15,5 +33,68 @@ Definition incr_spec :=
         SEP(`(data_at Ews tint (repinj tint z) ctr);`(data_at Ews (tptr tint) ctr addof_ctr))
   POST [tint]
         PROP()
+        LOCAL( temp ret_temp (Vint z))
+        SEP(`(data_at Ews tint (repinj tint z) ctr);`(data_at Ews (tptr tint) ctr addof_ctr)).
+
+Definition incr_spec :=
+ DECLARE _incr
+  WITH z : int, addof_ctr: val, ctr: val
+  PRE  [] 
+        PROP ()
+        LOCAL()
+        SEP()
+  POST [tvoid]
+        PROP()
+        LOCAL()
+        SEP().
+
+Definition main_spec :=
+ DECLARE _main
+  WITH z0 : int, z1 : int, addof_ctr: val, ctr: val
+  PRE  [] 
+        PROP ()
+        LOCAL(gvar _ctr addof_ctr)
+        SEP(`(data_at Ews tint (repinj tint z0) ctr);`(data_at Ews (tptr tint) ctr addof_ctr))
+  POST [tint]
+        PROP()
         LOCAL( temp ret_temp (Vint (Int.repr 2)))
-        SEP(`(data_at Ews tint (repinj tint (Int.add (Int.add Int.zero Int.one) Int.one)) ctr);`(data_at Ews (tptr tint) ctr addof_ctr)).
+        SEP(`(data_at Ews tint (repinj tint z1) ctr);`(data_at Ews (tptr tint) ctr addof_ctr)).
+
+Definition Vprog : varspecs := (_ctr_mutex, )::(_ctr, tptr tint)::(_zero,tint)::nil.
+
+Definition Gprog : funspecs := 
+  reset_spec::incr_spec::read_spec::main_spec::nil.
+
+Definition main_spec :=
+ DECLARE _main
+  WITH z : int, addof_ctr: val, ctr: val
+  PRE  [] 
+        PROP ()
+        LOCAL(gvar _counter addof_ctr)
+        SEP(`(data_at Ews tint (repinj tint z) ctr);`(data_at Ews (tptr tint) ctr addof_ctr))
+  POST [tint]
+        PROP()
+        LOCAL( temp ret_temp (Vint (Int.one)))
+        SEP(`(data_at Ews tint (repinj tint z) ctr);`(data_at Ews (tptr tint) ctr addof_ctr)).
+
+Definition Vprog : varspecs := (_counter, tptr tint)::nil.
+
+Definition Gprog : funspecs := 
+  main_spec::nil.
+
+Lemma body_main:  semax_body Vprog Gprog f_main main_spec.
+Proof.
+ start_function.
+ name ctr_ _counter.
+ forward.
+ eapply semax_seq.
+ eapply semax_post.
+ 
+ forward.
+ 
+ forward_call' (z, addof_ctr, ctr).
+ forward_call' (Int.zero, addof_ctr, ctr).
+ forward_call' ((Int.add Int.zero Int.one), addof_ctr, ctr).
+ forward_call' ((Int.add (Int.add Int.zero Int.one) Int.one), addof_ctr, ctr) r0.
+ forward.
+Qed.
